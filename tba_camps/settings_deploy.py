0eb2afdb148432b1d679a31a3d99e108a3b5b4d2
Luca De Feo <dfl@deagol>
2015-01-07 02:43:23 +0100

    SMTP configuration and settings file env vars cleanup

--- tba_camps/settings_deploy.py
+++ tba_camps/settings_deploy.py
@@ -5,11 +5,15 @@ from settings import *
 import dj_database_url
 
 DEBUG = False
-HOST_NAME = os.environ['HOST_NAME']
+HOST_NAME = os.environ.get('HOST_NAME') or 'localhost'
 ALLOWED_HOSTS = [ HOST_NAME ]
 HOST = 'http://%s' % HOST_NAME
 
-DATABASES['default'] =  dj_database_url.config()
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] =  dj_database_url.config()
+else:
+    DATABASES = {}
+    print 'Warning: no database configured. Set DATABASE_URL environment variable.'
 
 STATIC_ROOT = BASE_DIR + '/static/django/'
 MEDIA_ROOT = BASE_DIR + '/upload/'
@@ -17,8 +21,16 @@ MEDIA_ROOT = BASE_DIR + '/upload/'
 STATIC_URL = '/django/'
 
 # Mail settings
-#EMAIL_BACKEND = ''
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
+EMAIL_HOST_USER = os.environ.get('MAIL_USER') or ''
+EMAIL_HOST_PASSWORD = os.environ.get('MAIL_PASSWORD') or ''
+EMAIL_HOST = os.environ.get('MAIL_HOST') or 'localhost'
+EMAIL_PORT = os.environ.get('MAIL_PORT') or '25'
+EMAIL_USE_SSL = os.environ.get('MAIL_USE_SSL') in ['true', 'True', 'yes', 'Y']
+EMAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') in ['true', 'True', 'yes', 'Y'] and not EMAIL_USE_SSL
+
+FROM_EMAIL = os.environ.get('MAIL_FROM') or 'Camps de basket TBA <tba@camp-basket.com>'
 
 # Pieces PDF, Word, etc.
 PIECES = {
