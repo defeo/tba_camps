1b33f08c25a321770985501426e51cb8054fc19f
Luca De Feo <defeo@pippin>
2018-02-04 04:16:33 +0100

    Backed up Message model

--- tba_camps/management/commands/inscriptions_backup.py
+++ tba_camps/management/commands/inscriptions_backup.py
@@ -2,10 +2,10 @@ from django.core.management.base import BaseCommand, CommandError
 from django.core import serializers
 import tarfile, io
 from datetime import datetime
-from tba_camps.models import Dossier, Stagiaire, Semaine, Hebergement, Formule
+from tba_camps.models import Dossier, Stagiaire, Semaine, Hebergement, Formule, Message
 from django.conf import settings
 
-_models = [Dossier, Stagiaire, Semaine, Hebergement, Formule]
+_models = [Dossier, Stagiaire, Semaine, Hebergement, Formule, Message]
 
 class Command(BaseCommand):
     help = 'Backup model data (and uploads directory) in a tar file.'
