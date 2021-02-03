4bd4e75ec358f19892c7dc60221b6fea0035347c
Luca De Feo <defeo@merry>
2021-02-03 21:31:44 +0100

    Forgot to update dossier ressource

--- tba_camps/resources.py
+++ tba_camps/resources.py
@@ -97,7 +97,7 @@ class StagiaireResource(resources.ModelResource):
 class DossierResource(resources.ModelResource):
     lien = fields.Field()
     stagiaires = fields.Field()
-    prix_backpacks = fields.Field(column_name='prix sacs a dos')
+    prix_swag = fields.Field(column_name='prix swag (sacs a dos, etc.)')
     prix_total = fields.Field()
     acompte_total = fields.Field()
     reste = fields.Field()
@@ -117,7 +117,7 @@ class DossierResource(resources.ModelResource):
         export_base = fields = [
             'nom', 'prenom', 'stagiaires',
             'hebergement', 'prix_hebergement',
-            'supplement', 'motif', 'prix_backpacks',
+            'supplement', 'motif', 'prix_swag',
             'prix_total', 'acompte_total', 'reste',
             'mode', 'mode_solde',
             'email', 'tel', 'adresse', 'cp', 'ville',
@@ -152,8 +152,8 @@ class DossierResource(resources.ModelResource):
     def dehydrate_supplement(self, inscr):
         return inscr.supplement if inscr.supplement else ''
         
-    def dehydrate_prix_backpacks(self, inscr):
-        return inscr.prix_backpacks()
+    def dehydrate_prix_swag(self, inscr):
+        return inscr.prix_swag()
     
     def dehydrate_prix_total(self, inscr):
         return inscr.prix_total()
